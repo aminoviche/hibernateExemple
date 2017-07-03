287eb8fdc04e5b671838411975c490cfc768b3d4
ddd <ddd>
2017-07-03 23:28:21 +0100

    test

--- src/test/java/com/mkyong/common/AppTest.java
+++ src/test/java/com/mkyong/common/AppTest.java
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-package com.mkyong.common;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest  
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-}
-=======
 package com.mkyong.common;
 
 import junit.framework.Test;
@@ -50,30 +10,29 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-    /**
+    /**  
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )  
+    public AppTest( String testName )
     {
-        super( testName );
+        super( testName );  
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
+    public static Test suite()   
     {
-        return new TestSuite( AppTest.class );
+        return new  TestSuite( AppTest.class );
     }
 
     /**
-     * Rigourous Test :-)
+     * Rigourous Test  :-)
      */
     public void testApp()
     {
         assertTrue( true );
     }
 }
->>>>>>> branch 'master' of https://github.com/aminoviche/hibernateExemple.git
