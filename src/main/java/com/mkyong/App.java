f5e9dad02c03d4924607b9f21192707a574360e0
ddd <ddd>
2017-07-03 23:14:02 +0100

    test commit dev

--- src/main/java/com/mkyong/App.java
+++ src/main/java/com/mkyong/App.java
@@ -17,6 +17,7 @@ import com.mkyong.util.HibernateUtil;
  */
 public class App {
 	public static void main(String[] args) {
+		System.out.println("test");
 		System.out.println("Hibernate one to many (Annotation)");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 
